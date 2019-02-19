fd479d1ed8858165467b027fcbec2d56b88ebe09
Adrian Newby <acanewby@yahoo.com>
2019-02-19 08:00:58 +0000

    WIP

---
+++ nexus/configure-and-launch-nexus.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# The following env vars should be defined by the source image
+echo "===================================================="
+echo "Nexus environment"
+echo "----------------------------------------------------"
+echo "SONATYPE_DIR : ${SONATYPE_DIR}"
+echo "SONATYPE_WORK: ${SONATYPE_WORK}"
+echo "NEXUS_HOME   : ${NEXUS_HOME}"
+echo "NEXUS_DATA   : ${NEXUS_DATA}"
+echo "===================================================="
+
+while [ ! -d ${NEXUS_DATA}/log ]; do
+    echo "Waiting for Nexus data volume mount ..."
+    sleep 2
+done
+
+# Generate SSL keystore
+NEXUS_HOST=nexus
+NEXUS_PORT=8443
+JKS_PW=password
+
+echo "Making our keystore in NEXUS_DATA/etc/ssl ..."
+mkdir -p ${NEXUS_DATA}/etc/ssl
+
+keytool -genkeypair \
+    -keystore ${NEXUS_DATA}/etc/ssl/keystore.jks -storepass ${JKS_PW} \
+    -keypass ${JKS_PW} -alias jetty -keyalg RSA -keysize 4096 -validity 3650 \
+    -dname "CN=${NEXUS_HOST}, OU=acanewby, O=Sonatype, L=Unspecified, ST=Unspecified, C=US" \
+    -ext "BC=ca:true"
+
+keytool -list -keystore ${NEXUS_DATA}/etc/ssl/keystore.jks -storepass ${JKS_PW}
+
+# Trust the certificate
+keytool -printcert -rfc -sslserver ${NEXUS_HOST}:${NEXUS_PORT} > /etc/pki/ca-trust/source/anchors/${NEXUS_HOST}.crt
+sudo update-ca-trust
+
+echo "Configuring Sonatype Nexus ..."
+cp /scratch/nexus.properties ${NEXUS_DATA}/etc
+
+echo "Launching Sonatype Nexus ..."
+${SONATYPE_DIR}/start-nexus-repository-manager.sh
